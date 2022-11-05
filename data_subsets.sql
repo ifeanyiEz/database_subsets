263d27628ed845f2dce20ed9c4d98edfb4d618f3
Ezugworie Ifeanyi Ofor <ofotronics.2010@gmail.com>
2022-11-05 11:48:47 +0100

    Rows and colums with amount less than 500

--- data_subsets.sql
+++ data_subsets.sql
@@ -11,4 +11,12 @@ WHERE gloss_amt_usd >= 1000
 LIMIT 5;
 
 
+/*First 10 rows and all columns from the orders table that have a total_amt_usd less than 500*/
+
+SELECT *
+FROM orders
+WHERE total_amt_usd < 500
+LIMIT 10;
+
+
 
